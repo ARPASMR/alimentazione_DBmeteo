095a10f0f3e59817bd4f2f8ed4ae4ea56bbba81a
mariaranci <31917865+mariaranci@users.noreply.github.com>
2021-12-31 10:09:13 +0100

    aggiornamento per acquisizione 2022

--- aggiornamento_ftp_rem2.R
+++ aggiornamento_ftp_rem2.R
@@ -427,6 +427,7 @@ while( i <= length(nomefile) ){
       nel_2019  <- NULL 
       nel_2020  <- NULL
       nel_2021  <- NULL
+      nel_2022  <- NULL
       nel_1987  <- which(format(tempo[indice],"%Y") == "1987")
       nel_1988  <- which(format(tempo[indice],"%Y") == "1988")
       nel_1989  <- which(format(tempo[indice],"%Y") == "1989")
@@ -462,9 +463,10 @@ while( i <= length(nomefile) ){
       nel_2019  <- which(format(tempo[indice],"%Y") == "2019")
       nel_2020  <- which(format(tempo[indice],"%Y") == "2020")
       nel_2021  <- which(format(tempo[indice],"%Y") == "2021")
+      nel_2022  <- which(format(tempo[indice],"%Y") == "2022")
 ##### @@@@@@@@@@ 
 # CICLO SUGLI ANNI  
-      for (anno in seq(1987,2021, by=1)) {
+      for (anno in seq(1987,2022, by=1)) {
 ##### @@@@@@@@@@ !!!!!!!!
         index <- NULL
         index <- indice[eval(parse(text=paste("nel_",anno,sep="")))]
